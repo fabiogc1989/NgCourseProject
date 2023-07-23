51a9a0121da383a85d4f0c2b2f138a76dc68abc2
Fabio Coimbra <fabiogc1989@gmail.com>
2023-07-23 18:47:20 +0100

    ngFor recipes list with dummy data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  public recipes: Recipe[] = [
+    new Recipe("Recipe test", "description test", "https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg"),
+    new Recipe("Recipe test", "description test", "https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg")
+  ];
 
+  constructor() { }
 }
