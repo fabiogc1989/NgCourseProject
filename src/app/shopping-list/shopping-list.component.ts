c9bad07e840a7ec6e0b7dc4e87ea8cd2fdc8aa38
Fabio Coimbra <fabiogc1989@gmail.com>
2023-10-29 13:22:48 +0000

    Allow the user to add ingredients to shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -5,7 +5,7 @@ import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
@@ -13,4 +13,7 @@ export class ShoppingListComponent {
     new Ingredient('Tomatoes', 10),
   ];
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
