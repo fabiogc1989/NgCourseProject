9b5c0b4abd561d51d306c8ddf807553c5b89b094
Fabio Coimbra <fabiogc1989@gmail.com>
2024-01-27 23:42:59 +0000

    add ingredients to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Recipe test',
       'description test',
-      'https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg'
+      'https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Chips', 20)]
     ),
     new Recipe(
       'Recipe 2 test',
       'description 2 test',
-      'https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg'
+      'https://www.inspiredtaste.net/wp-content/uploads/2023/01/Easy-Bean-Salad-Recipe-Video.jpg',
+      [new Ingredient('Tomatoes', 6)]
     ),
   ];
 
