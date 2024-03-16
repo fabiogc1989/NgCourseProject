2b99fc7b959f1a40cf9dd3b649c805dd2204baf5
Fabio Coimbra <fabiogc1989@gmail.com>
2024-03-16 23:56:26 +0000

    setup app routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    { path: 'recipes', component: RecipeListComponent },
+    { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports:[RouterModule]
+})
+export class AppRoutingModule{}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
+import { RecipeService } from './recipes/recipe.service';
 
 @NgModule({
   declarations: [
@@ -26,8 +28,12 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, FormsModule],
-  providers: [ShoppingListService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
