a66cd0a8b8e715c29e20a000fa3efaa04d2152ac
Fabio Coimbra <fabiogc1989@gmail.com>
2024-07-28 15:50:25 +0100

    add signup feature

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
+import { AuthService } from "./auth.service";
+
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
@@ -8,12 +10,30 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
     isLoginMode: boolean = false;
 
+    constructor(private authService: AuthService){}
+
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        if(form.invalid){
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+        if (this.isLoginMode) {
+            // ...
+        } else {
+            this.authService
+            .signup(email, password)
+            .subscribe(authData => {
+                console.log(authData);
+            }, error => {
+                console.log(error);
+            });
+        }
+        
         form.reset()
     }
 }
