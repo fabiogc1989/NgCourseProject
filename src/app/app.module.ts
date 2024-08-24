85c53dfbffc4ef0e416457c01db143646d0b2740
Fabio Coimbra <fabiogc1989@gmail.com>
2024-08-24 13:22:59 +0100

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,22 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shoppinglist.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
