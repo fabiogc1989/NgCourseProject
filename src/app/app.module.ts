7199b9bf73b324a7e2094c11f76c9b0a3b86c4e1
Fabio Coimbra <fabiogc1989@gmail.com>
2024-09-04 22:53:06 +0100

    shopping list lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListModule } from './shopping-list/shoppinglist.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
@@ -23,7 +21,6 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AppRoutingModule,
     AuthModule,
-    ShoppingListModule,
     SharedModule,
     CoreModule
   ],
